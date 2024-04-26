28ffb4007fca1e25a8ae8c0db942ed6bdedc5736
Cesar Galvis Leon <cgalvisleon@gmail.com>
2024-04-26 13:44:10 -0500

    Update .gitignore and clean cache

--- project/api/main.go
+++ project/api/main.go
@@ -5,12 +5,38 @@ import "fmt"
 func main() {
 	var a int = 5
 	var b int = 6
-	var c int = a + b
+	// var c int = a + b
 
-	fmt.Println(c)
+	c := Suma(a, b)
+	d := Resta(a, b)
+	e, f := SumaResta(a, b)
+	g := Potencia(a, b)
+
 	fmt.Println("Hello, World!")
+	fmt.Sprintf("La suma de %d + %d es %d", a, b, c)
+	fmt.Sprintf("La resta de %d - %d es %d", a, b, d)
+	fmt.Sprintf("La suma de %d + %d es %d y la resta de %d - %d es %d", a, b, e, a, b, f)
+	fmt.Sprintf("La potencia de %d elevado a %d es %d", a, b, g)
+}
+
+func Suma(a int, b int) int {
+	return a + b
 }
 
 func Resta(a int, b int) int {
 	return a - b
+}
+func SumaResta(a int, b int) (int, int) {
+	c := Suma(a, b)
+	d := Resta(a, b)
+
+	return c, d
+}
+
+func Potencia(a int, b int) int {
+	for i := 1; i < b; i++ {
+		a = a * a
+	}
+
+	return a
 }
